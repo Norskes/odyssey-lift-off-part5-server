541086467b57fdb4fe35f3595810a7f54ed1d251
Ilya Ivanov <ilya.ivanov@godigitalmg.com>
2022-09-29 00:15:27 +0300

    Fix deprecation directive

--- src/schema.js
+++ src/schema.js
@@ -36,9 +36,9 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int  @deprecated(reason: "Use durationInSeconds")
     "The track's full duration, in seconds"
-    durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
